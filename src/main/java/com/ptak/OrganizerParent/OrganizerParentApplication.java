553286804f6aa2a8fba749fd03fc66b74b467ef0
Arkadiusz.Ptak <arkadiusz.ptak@cybercom.com>
2020-04-21 16:37:57 +0200

    initial commit

---
+++ src/main/java/com/ptak/OrganizerParent/OrganizerParentApplication.java
@@ -0,0 +1,13 @@
+package com.ptak.OrganizerParent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizerParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizerParentApplication.class, args);
+	}
+
+}
